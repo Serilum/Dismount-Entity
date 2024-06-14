803c2d6b9f150f239f920e522b9971475d02036b
Rick South <rick@natamus.com>
2024-06-14 05:35:24 +0200

    Dismount Entity source code for 1.21.0.

---
+++ NeoForge/src/main/java/com/natamus/dismountentity/neoforge/events/NeoForgeDismountEvent.java
@@ -0,0 +1,17 @@
+package com.natamus.dismountentity.neoforge.events;
+
+import com.natamus.dismountentity.events.DismountEvent;
+import net.minecraft.world.InteractionResult;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeDismountEvent {
+	@SubscribeEvent
+	public static void onPlayerInteract(PlayerInteractEvent.EntityInteract e) {
+		if (DismountEvent.onPlayerInteract(e.getEntity(), e.getLevel(), e.getHand(), e.getTarget(), null).equals(InteractionResult.SUCCESS)) {
+			e.setCanceled(true);
+		}
+	}
+}
