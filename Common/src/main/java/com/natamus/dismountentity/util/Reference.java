ee0ad70e2facec7b49b1104010280c932066440a
Rick South <rick@natamus.com>
2024-07-15 23:12:37 +0200

    (v3.4) Fixed incorrectly set Minecraft version.

--- Common/src/main/java/com/natamus/dismountentity/util/Reference.java
+++ Common/src/main/java/com/natamus/dismountentity/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.dismountentity.util;
 public class Reference {
 	public static final String MOD_ID = "dismountentity";
 	public static final String NAME = "Dismount Entity";
-	public static final String VERSION = "3.3";
+	public static final String VERSION = "3.4";
 	public static final String ACCEPTED_VERSIONS = "[1.20.6]";
 }
