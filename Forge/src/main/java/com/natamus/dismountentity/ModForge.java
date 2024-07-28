199ec666360d2c8b4bc8018decfbd9308e20bbb8
Rick South <rick@natamus.com>
2024-07-28 07:36:33 +0200

    (v3.5) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Forge/src/main/java/com/natamus/dismountentity/ModForge.java
+++ Forge/src/main/java/com/natamus/dismountentity/ModForge.java
@@ -1,6 +1,7 @@
 package com.natamus.dismountentity;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.dismountentity.forge.config.IntegrateForgeConfig;
 import com.natamus.dismountentity.forge.events.ForgeDismountEvent;
 import com.natamus.dismountentity.util.Reference;
@@ -15,6 +16,10 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModForge {
 	
 	public ModForge() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 		modEventBus.addListener(this::loadComplete);
 
