57f37299f4473b225938a6a96b370d798b12f5db
Rick South <rick@natamus.com>
2024-07-28 07:36:40 +0200

    (v3.5) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- NeoForge/src/main/java/com/natamus/dismountentity/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/dismountentity/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.dismountentity;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.dismountentity.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.dismountentity.neoforge.events.NeoForgeDismountEvent;
 import com.natamus.dismountentity.util.Reference;
@@ -14,6 +15,10 @@ import net.neoforged.neoforge.common.NeoForge;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
