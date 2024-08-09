b60ed5ddd7493571afa028f2185e840c67e85a2b
Rick South <rick@natamus.com>
2024-08-09 08:27:05 +0200

    Dismount Entity source code for 1.21.1.

---
+++ Fabric/src/main/java/com/natamus/dismountentity/ModFabric.java
@@ -0,0 +1,35 @@
+package com.natamus.dismountentity;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
+import com.natamus.dismountentity.events.DismountEvent;
+import com.natamus.dismountentity.util.Reference;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.UseEntityCallback;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		UseEntityCallback.EVENT.register((player, world, hand, entity, hitResult) -> {
+			return DismountEvent.onPlayerInteract(player, world, hand, entity, hitResult);
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
