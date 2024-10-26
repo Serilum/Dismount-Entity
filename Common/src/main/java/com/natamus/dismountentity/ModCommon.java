fe83356b602d561e6c857e51e0f3e3b3bbd401a2
Rick South <rick@natamus.com>
2024-10-26 15:18:23 +0200

    Dismount Entity source code for 1.21.3.

---
+++ Common/src/main/java/com/natamus/dismountentity/ModCommon.java
@@ -0,0 +1,16 @@
+package com.natamus.dismountentity;
+
+
+import com.natamus.dismountentity.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
