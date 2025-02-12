adbf0d8c9fc0f0921d8aa4ec24238eb342c326dd
Rick South <rick@natamus.com>
2025-02-12 23:37:29 +0100

    (v3.6) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/dismountentity/forge/events/ForgeDismountEvent.java
+++ Forge/src/main/java/com/natamus/dismountentity/forge/events/ForgeDismountEvent.java
@@ -4,12 +4,10 @@ import com.natamus.dismountentity.events.DismountEvent;
 import net.minecraft.world.InteractionResult;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
-@EventBusSubscriber
 public class ForgeDismountEvent {
 	@SubscribeEvent
-	public void onPlayerInteract(PlayerInteractEvent.EntityInteract e) {
+	public static void onPlayerInteract(PlayerInteractEvent.EntityInteract e) {
 		if (DismountEvent.onPlayerInteract(e.getEntity(), e.getLevel(), e.getHand(), e.getTarget(), null).equals(InteractionResult.SUCCESS)) {
 			e.setCanceled(true);
 		}
--- NeoForge/src/main/java/com/natamus/dismountentity/neoforge/events/NeoForgeDismountEvent.java
+++ NeoForge/src/main/java/com/natamus/dismountentity/neoforge/events/NeoForgeDismountEvent.java
@@ -4,9 +4,7 @@ import com.natamus.dismountentity.events.DismountEvent;
 import net.minecraft.world.InteractionResult;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
 
-@EventBusSubscriber
 public class NeoForgeDismountEvent {
 	@SubscribeEvent
 	public static void onPlayerInteract(PlayerInteractEvent.EntityInteract e) {
